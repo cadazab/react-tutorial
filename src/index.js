7284672ec571f21f5c6461294d08c367dd48302f
kmi <kmi@iMac-de-Ini.fritz.box>
2017-07-13 01:08:32 +0200

    tabs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Tabs, Tab} from 'material-ui/Tabs';
 import './index.css'
+import Product from './tabs/Product'
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
 
 /* If a Component only consist of a render method, it can be replaced by a
 functional component  */
@@ -123,9 +138,102 @@ function calculateWinner(squares) {
   return null;
 }
 
+class Test extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'a',
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      value: value,
+    });
+  };
+
+
+  render(){
+    return(
+      <Tabs
+        value={this.state.value}
+        onChange={this.handleChange}
+      >
+        <Tab label="Product" value="a">
+          <div>
+              <Product />
+          </div>
+        </Tab>
+        <Tab label="Group" value="b">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Rule" value="c">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab C</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Properties" value="d">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Test Cases" value="e">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Logging" value="f">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+      </Tabs>
+    );
+  }
+}
+
+function App(){
+    return (
+      <MuiThemeProvider>
+        <Test />
+      </MuiThemeProvider>
+    );
+}
+
+
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <div>
+    <App />
+  </div>,
   document.getElementById('root')
 );
---
+++ src/tabs/Product.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+class Product extends React.Component {
+
+  render(){
+    return(
+        <div>
+          <p>
+            lol
+          </p>
+        </div>
+    );
+  }
+}
+
+export default Product;
