6c587249604e38e7916dae7a953b73726b4ee5e4
cadazab <cadazab@gmail.com>
2017-07-14 14:05:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,144 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {Tabs, Tab} from 'material-ui/Tabs';
 import './index.css'
 import Product from './tabs/Product'
+import { Tab } from 'semantic-ui-react'
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
-const styles = {
-  headline: {
-    fontSize: 24,
-    paddingTop: 16,
-    marginBottom: 12,
-    fontWeight: 400,
-  },
-};
-
-/* If a Component only consist of a render method, it can be replaced by a
-functional component  */
-function Square(props){
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-}
-
-class Board extends React.Component {
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-       history: history.concat([{
-        squares: squares,
-      }]),
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const winner = calculateWinner(current.squares);
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+const panes = [
+  { menuItem: 'Product', render: () => <Tab.Pane className='tab'><Product /></Tab.Pane> },
+  { menuItem: 'Group', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+  { menuItem: 'Rule', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+  { menuItem: 'Properties', render: () => <Tab.Pane>Tab 4 Content</Tab.Pane> },
+  { menuItem: 'Test Cases', render: () => <Tab.Pane>Tab 5 Content</Tab.Pane> },
+  { menuItem: 'Logging', render: () => <Tab.Pane>Tab 6 Content</Tab.Pane> },
+]
 
 class Test extends React.Component {
 
@@ -149,85 +22,12 @@ class Test extends React.Component {
     };
   }
 
-  handleChange = (value) => {
-    this.setState({
-      value: value,
-    });
-  };
-
-
+  
   render(){
     return(
-      <Tabs
-        value={this.state.value}
-        onChange={this.handleChange}
-      >
-        <Tab label="Product" value="a">
-          <div>
-              <Product />
-          </div>
-        </Tab>
-        <Tab label="Group" value="b">
-          <div>
-            <h2 style={styles.headline}>Controllable Tab B</h2>
-            <p>
-              This is another example of a controllable tab. Remember, if you
-              use controllable Tabs, you need to give all of your tabs values or else
-              you wont be able to select them.
-            </p>
-          </div>
-        </Tab>
-        <Tab label="Rule" value="c">
-          <div>
-            <h2 style={styles.headline}>Controllable Tab C</h2>
-            <p>
-              This is another example of a controllable tab. Remember, if you
-              use controllable Tabs, you need to give all of your tabs values or else
-              you wont be able to select them.
-            </p>
-          </div>
-        </Tab>
-        <Tab label="Properties" value="d">
-          <div>
-            <h2 style={styles.headline}>Controllable Tab B</h2>
-            <p>
-              This is another example of a controllable tab. Remember, if you
-              use controllable Tabs, you need to give all of your tabs values or else
-              you wont be able to select them.
-            </p>
-          </div>
-        </Tab>
-        <Tab label="Test Cases" value="e">
-          <div>
-            <h2 style={styles.headline}>Controllable Tab B</h2>
-            <p>
-              This is another example of a controllable tab. Remember, if you
-              use controllable Tabs, you need to give all of your tabs values or else
-              you wont be able to select them.
-            </p>
-          </div>
-        </Tab>
-        <Tab label="Logging" value="f">
-          <div>
-            <h2 style={styles.headline}>Controllable Tab B</h2>
-            <p>
-              This is another example of a controllable tab. Remember, if you
-              use controllable Tabs, you need to give all of your tabs values or else
-              you wont be able to select them.
-            </p>
-          </div>
-        </Tab>
-      </Tabs>
+      <Tab panes={panes} />
     );
   }
-}
-
-function App(){
-    return (
-      <MuiThemeProvider>
-        <Test />
-      </MuiThemeProvider>
-    );
 }
 
 
@@ -235,7 +35,7 @@ function App(){
 
 ReactDOM.render(
   <div>
-    <App />
+    <Test />
   </div>,
   document.getElementById('root')
 );
